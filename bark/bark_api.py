59a1374ad591aeed5366b048db61fe39150baba2
theMladyPan <stanislav@rubint.sk>
2023-10-10 16:34:10 +0200

    wip

--- bark/bark_api.py
+++ bark/bark_api.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+time_start = time.perf_counter()
 from scipy.io.wavfile import write as write_wav
 import sys
 import argparse
 import logging
 import os
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 # parse command line arguments
@@ -37,6 +39,37 @@ parser.add_argument(
 # parse
 args = parser.parse_args()
 
+if args.text == "":
+    print("Input text prompt: ")
+
+    # read stdin for text prompt
+    text_prompt = sys.stdin.read()
+else:
+    text_prompt = args.text
+    
+# sanitize filename for output
+output_filename = args.output
+for char in ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]:
+    output_filename.replace(char, "_")
+
+if not output_filename.endswith(".wav"):
+    output_filename += ".wav"
+
+log.info(f"Text prompt: {text_prompt}")
+
+sentences = []
+if len(text_prompt) > 20:
+    log.warning("Text prompt is longer than 20 characters, this may take a while...")
+    import nltk
+    nltk.download('punkt')  # Download the required punkt tokenizer if you haven't already
+
+    from nltk.tokenize import sent_tokenize
+
+    sentences = sent_tokenize(text_prompt)
+
+    log.info(f"Split into {len(sentences)} sentences")
+    log.debug(sentences)
+
 if args.small:
     os.environ["SUNO_USE_SMALL_MODELS"] = "1"
     log.info("Using small models")
@@ -47,22 +80,60 @@ log.info("Loading models...")
 # download and load all models
 preload_models()
 
-if args.text == "":
-    print("Input text prompt: ")
+sentence_filenames = []
+if len(sentences) > 0:
+    for i, sentence in enumerate(sentences, start=1):
+        log.info(f"Generating audio for sentence {i}...")
+        # generate audio from text
+        if i == 1:
+            # generate first prompt with history
+            full_gen, audio_array = generate_audio(
+                sentence,
+                output_full=True
+            )
+        else:
+            # generate subsequent prompts with history
+            full_gen, audio_array = generate_audio(
+                sentence,
+                history_prompt=full_gen,
+                output_full=True
+            )
+    
+        intermediate_filename = output_filename.replace(".wav", f"_{i}_{len(sentences)}.wav")
+        sentence_filenames.append(intermediate_filename)
 
-    # read stdin for text prompt
-    text_prompt = sys.stdin.read()
+        log.info(f"Saving audio to {intermediate_filename}...")
+        # save audio to disk
+        write_wav(intermediate_filename, SAMPLE_RATE, audio_array)
+        
 else:
-    text_prompt = args.text
-    
-log.info(f"Text prompt: {text_prompt}")
+    log.info("Generating audio...")
+    # generate audio from text
+    audio_array = generate_audio(text_prompt)
 
-# generate audio from text
-audio_array = generate_audio(text_prompt)
+    log.info(f"Saving audio to {output_filename}...")
+    # save audio to disk
+    write_wav(output_filename, SAMPLE_RATE, audio_array)
 
-if not args.output.endswith(".wav"):
-    args.output += ".wav"
+# finally merge all the audio files
+if len(sentences) > 0:
+    log.info("Merging audio files...")
+    from pydub import AudioSegment
+
+    combined = AudioSegment.empty()
+    for filename in sentence_filenames:
+        combined += AudioSegment.from_wav(filename)
+
+    # save as mp3
+    output_filename = output_filename.replace(".wav", ".mp3")
     
-log.info(f"Saving audio to {args.output}...")
-# save audio to disk
-write_wav(args.output, SAMPLE_RATE, audio_array)
+    combined.export(output_filename, format="mp3")
+    log.info(f"Saved merged audio to {output_filename}")
+    
+    log.info("Deleting temporary files...")
+    # delete temporary files
+    for filename in sentence_filenames:
+        log.debug(f"Deleting {filename}")
+        os.remove(filename)             
+             
+log.info(f"Done! Took {time.perf_counter() - time_start:.3f} seconds")
